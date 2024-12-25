eb2d695139fe536c09593fa1d23b5ca0f37b9ad0
akyoshid <akyoshid@student.42tokyo.jp>
2024-12-25 15:53:49 +0000

    [RFCT] follow the norm

--- src/proc_mlx_new_window.c
+++ src/proc_mlx_new_window.c
@@ -6,7 +6,7 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 14:55:54 by akyoshid          #+#    #+#             */
-/*   Updated: 2024/12/25 15:30:40 by akyoshid         ###   ########.fr       */
+/*   Updated: 2024/12/25 15:53:15 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	proc_mlx_new_window(t_data *data)
 {
 	data->win = mlx_new_window(data->mlx,
 			data->map_data.x_count * 64,
-			data->map_data.y_count * 64,"so_long");
+			data->map_data.y_count * 64, "so_long");
 	if (data->win == NULL)
 		proc_mlx_err(data, SL_ERR_MLX_NEW_WIN,
 			"mlx_new_window: Failed to create a new window\n");
